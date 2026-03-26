8da20f51d2c261b6eb4f990f4cfddad82eab754c
veda1230 <vv1576@srmist.edhu.in>
2026-03-26 15:23:03 +0530

    UC2 implement

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,12 @@
 public class HelloApp {
-    public static void main(String[] var0) {
-      System.out.println("Hello World");
-   }
+    public static void main(String[] args) {
+        // Check if the array actually has data in it first
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            // Fallback if the user forgets to type a name
+            System.out.println("Hello, World!");
+        }
+    }
 }
